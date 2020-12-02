e9034eb3af5b26623357c9612224dd7dc527c93f
MarllonCampos <marllondcsp@gmail.com>
2020-12-02 00:56:25 -0300

    Começado a colocar o javascript na pagina para transforma-lo em pagina nao estatica

---
+++ scripts/cadastro.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#btn-cadastrar')
+
+
+const nome = document.querySelector('input#nome-cadastro').value
+
+const telefone = document.querySelector('input#tel-cadastro')
+const senha = document.querySelector('input#senha-cadastro')
+const confsenha = document.querySelector('input#confsenha-cadastro')
+
+console.log({ nome, telefone, senha, confsenha, button })
