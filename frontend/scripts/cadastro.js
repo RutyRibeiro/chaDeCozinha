39da0d8d3f420c29fffba04af5b0818e24b33e57
MarllonCampos <marllondcsp@gmail.com>
2020-12-03 14:31:20 -0300

    Adicionado a funcionalidade de máscara no input de telefone

--- frontend/scripts/cadastro.js
+++ frontend/scripts/cadastro.js
@@ -1,14 +1,19 @@
+const telefone = document.querySelector('input#tel-cadastro');
+VMasker(telefone).maskPattern("(99) 9 9999-9999");
+
 const button = document.querySelector('#btn-cadastrar')
 button.addEventListener('click', cadastrar)
 
 function limpaCampos(parametro) {
     parametro.value = ''
 }
+
 function cadastrar() {
     const nome = document.querySelector('input#nome-cadastro')
-    const telefone = document.querySelector('input#tel-cadastro')
     const senha = document.querySelector('input#senha-cadastro')
     const confsenha = document.querySelector('input#confsenha-cadastro')
+
+
 
     if (nome.value == '' || telefone.value == '' || senha.value == '' || confsenha.value == '') {
         alert('Preencha todos os campos!')
@@ -25,6 +30,9 @@ function cadastrar() {
     }
     else {
         const nomeBD = nome.value.trim()
+        const telefoneBD = (telefone.value.replaceAll(' ', '')).replace(/[^0-9]/g, '')
         console.log(nomeBD)
+        console.log(senha.value);
+        console.log(telefoneBD);
     }
 }
--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -1,6 +1,11 @@
-const button = document.querySelector('#btn-login')
-button.addEventListener('click', login)
+const telefone = document.querySelector('input#tel-login');
+VMasker(telefone).maskPattern("(99) 9 9999-9999");
 
+const button = document.querySelector('#btn-logar');
+
+
+
+button.addEventListener('click', login)
 
 function limpaCampos(parametro) {
     parametro.value = ''
@@ -8,20 +13,27 @@ function limpaCampos(parametro) {
 
 
 function login() {
-    const telefone = document.querySelector('input#tel-login')
     const senha = document.querySelector('input#senha-login')
+    const telefoneBD = (telefone.value.replaceAll(' ', '')).replace(/[^0-9]/g, '')
 
-    if (telefone.value == '' || senha.value == '') {
+    if (telefoneBD == '' || senha.value == '') {
         alert('Preencha todos os campos!')
     }
-    else if ((telefone.value).length !== 12) {
+    else if (telefoneBD < 12) {
+        console.log(telefoneBD.length)
         alert('Por favor insira a quantidade correta de caracteres')
 
     }
     else if ((senha.value).length < 5) {
         alert('Senha muito curta!')
         limpaCampos(senha)
-        limpaCampos(confsenha)
+    } else {
+        const senhaBD = senha.value
+        const usuario = {
+            "senha": senhaBD,
+            "telefone": telefoneBD
+        }
+        console.log(usuario);
     }
 
 
