c22a9563d23d7bd601f92524f55f77ff966e0036
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-12-13 23:51:01 -0300

    botão escolher

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,11 +1,23 @@
+const main = document.querySelector('main')
+
 const carregaProdutos = async () => {
+    const loading = document.createElement('div')
+    loading.style.width = '90px'
+    loading.id = 'loading'
+    const loadingIcon = document.createElement('img')
+    loadingIcon.src = './img/loading-icon.svg'
+    loadingIcon.style.width = '100%'
+    loadingIcon.style.height = '100%'
+
+    loading.appendChild(loadingIcon)
+    main.appendChild(loading)
     const response = await axios.get('https://us-central1-casamento-thalita.cloudfunctions.net/app')
     const data = response.data
     criaContainerItems(data);
 }
 
 const criaContainerItems = async (json) => {
-    const main = document.querySelector('main')
+    const loading = document.querySelector('#loading')
     await json.map(obj => {
         const containerItems = document.createElement('div')
         containerItems.classList.add('container-items')
@@ -46,10 +58,25 @@ const criaContainerItems = async (json) => {
 
         const textDescription = document.createElement('p')
         textDescription.innerHTML = obj.descricao_produto;
+        
 
         productDescription.appendChild(textDescription)
 
         descriptionElements.appendChild(productDescription)
+
+
+        const buttons = document.createElement('div')
+        buttons.classList.add('buttons')
+
+
+        const choose = document.createElement('a')
+        choose.classList.add('choose')
+        choose.id = `product${obj.id_produto}`
+        choose.innerText='Escolher'
+        choose.addEventListener('click',() => {
+            sendConfirmBack(choose.id,userID)
+        })
+
 
         const sales = document.createElement('a')
         sales.classList.add('sales')
@@ -59,14 +86,29 @@ const criaContainerItems = async (json) => {
         sales.target = '_blank'
         sales.href = link
 
+        buttons.appendChild(choose)
+        buttons.appendChild(sales)
+
         containerItems.appendChild(imageDescription)
-        containerItems.appendChild(sales)
+        containerItems.appendChild(buttons)
         main.appendChild(containerItems)
     })
 
+    const backToTop = document.querySelector('.backToTop')
+    backToTop.classList.add('appear')
 
-
-
+  
+    loading.remove();
 }
 
 window.addEventListener('load', carregaProdutos)
+
+
+const sendConfirmBack = (productID, userID) => {
+    const id = productID.replace('product','')
+    console.log(userID,productID, id)
+}
+
+const verifyUser = () => {
+    
+}
